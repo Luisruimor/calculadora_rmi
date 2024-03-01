1d1419a0c7e568ebdd6deb05bff9a06e092f7885
Luis <luisruimore@gmail.com>
2024-03-01 18:59:59 +0100

    Cambio 1 y 2
    Cambio en la IP y en el puerto del cliente

--- Cliente.java
+++ Cliente.java
@@ -4,8 +4,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 public class Cliente {
-	private static final String IP = "192.168.1.111"; // Puedes cambiar a localhost
-	private static final int PUERTO = 1200; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
+	private static final String IP = "localhost"; // Puedes cambiar a localhost
+	private static final int PUERTO = 1100; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
 	
     public static void main(String[] args) throws RemoteException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry(IP, PUERTO);
